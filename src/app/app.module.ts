f4bdf3e493c324972a3484471b93d4e03f01452a
albertoAyala01 <aayala@apexsystems.com>
2024-03-20 15:37:16 -0600

    implementation of exercise 1. Validations with directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToggleButtonComponent } from './components/toggle-button/toggle-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToggleButtonComponent
   ],
   imports: [
     BrowserModule,
