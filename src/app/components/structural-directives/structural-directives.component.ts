94529006dce0d9d3f53156e8ac1d919d8ae42876
AlbertoAyala01 <aayala@apexsystems.com>
2024-03-22 18:17:27 -0600

    changing to primitive type instead of wrap class

--- src/app/components/structural-directives/structural-directives.component.ts
+++ src/app/components/structural-directives/structural-directives.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './structural-directives.component.scss'
 })
 export class StructuralDirectivesComponent {
-  letters: String[]= ["A","B","C","D"]
+  letters: string[]= ["A","B","C","D"]
 }
