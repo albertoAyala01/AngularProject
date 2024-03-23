8218044dcc301038bc8a529a5ab0f68a8a067445
AlbertoAyala01 <aayala@apexsystems.com>
2024-03-22 19:23:27 -0600

    changing approach to modify data instead of view

--- src/app/directives/capitalize.directive.ts
+++ src/app/directives/capitalize.directive.ts
@@ -6,7 +6,28 @@ import { Directive, ElementRef } from '@angular/core';
 export class CapitalizeDirective {
 
   constructor(private eleRef: ElementRef) {
-    eleRef.nativeElement.style.textTransform = 'capitalize';
+  }
+
+  ngAfterViewInit() {
+    this.capitalizeText();
+  }
+
+  capitalizeText():void{
+    let txtContent:string = this.eleRef.nativeElement.innerText
+
+    if(txtContent){
+      const textArray:string[]= txtContent.split(" ");
+      const totalWords:number = textArray.length;
+      let textCapitalized:string = "";
+
+      textArray.forEach((word:string, idx:number)=>{
+        textCapitalized += word.charAt(0).toUpperCase() +word.slice(1);
+        if( (idx+1) < totalWords){
+          textCapitalized+=" ";
+        }
+      })
+      this.eleRef.nativeElement.innerText = textCapitalized
+    }
   }
 
 }
