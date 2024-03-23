916eb093a81f11695da2ac22c7883a852b398906
AlbertoAyala01 <aayala@apexsystems.com>
2024-03-22 19:28:13 -0600

    formating code

--- src/app/directives/capitalize.directive.ts
+++ src/app/directives/capitalize.directive.ts
@@ -1,33 +1,31 @@
 import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
-  selector: '[capitalize]'
+  selector: '[capitalize]',
 })
 export class CapitalizeDirective {
-
-  constructor(private eleRef: ElementRef) {
-  }
+  constructor(private eleRef: ElementRef) {}
 
   ngAfterViewInit() {
     this.capitalizeText();
   }
 
-  capitalizeText():void{
-    let txtContent:string = this.eleRef.nativeElement.innerText
+  capitalizeText(): void {
+    let txtContent: string = this.eleRef.nativeElement.innerText;
 
-    if(txtContent){
-      const textArray:string[]= txtContent.split(" ");
-      const totalWords:number = textArray.length;
-      let textCapitalized:string = "";
+    if (txtContent) {
+      const textArray: string[] = txtContent.split(' ');
+      const totalWords: number = textArray.length;
+      let textCapitalized: string = '';
 
-      textArray.forEach((word:string, idx:number)=>{
-        textCapitalized += word.charAt(0).toUpperCase() +word.slice(1);
-        if( (idx+1) < totalWords){
-          textCapitalized+=" ";
+      textArray.forEach((word: string, idx: number) => {
+        textCapitalized += word.charAt(0).toUpperCase() + word.slice(1);
+        if (idx + 1 < totalWords) {
+          textCapitalized += ' ';
         }
-      })
-      this.eleRef.nativeElement.innerText = textCapitalized
+      });
+
+      this.eleRef.nativeElement.innerText = textCapitalized;
     }
   }
-
 }
